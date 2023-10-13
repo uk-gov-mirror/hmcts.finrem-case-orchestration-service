b6c459599fa836ce6994e71fda4ef173e668a1ad
Ruban <ruban72@hotmail.com>
2023-10-13 11:06:24 +0100

    DFR-2332: Update integration tests

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/integrationtest/handler/AmendApplicationAboutToSubmitHandlerIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/integrationtest/handler/AmendApplicationAboutToSubmitHandlerIntegrationTest.java
@@ -10,12 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.BaseTest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.AmendApplicationAboutToSubmitHandler;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.integrationtest.IntegrationTest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderService;
@@ -25,10 +25,6 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderService;
 @Slf4j
 @Category(IntegrationTest.class)
 public class AmendApplicationAboutToSubmitHandlerIntegrationTest extends BaseTest {
-
-    public static final String DOC_URL = "Test Document URL";
-    public static final String BINARY_URL = "Test Binary URL";
-    public static final String FILE_NAME = "Test File Name";
 
     @Autowired
     private ConsentOrderService consentOrderService;
@@ -40,24 +36,20 @@ public class AmendApplicationAboutToSubmitHandlerIntegrationTest extends BaseTes
     private AmendApplicationAboutToSubmitHandler amendApplicationAboutToSubmitHandler;
 
     @Test
-    public void shouldSetDefaultValues() throws Exception{
+    public void shouldSetDefaultValues() {
 
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response
             = amendApplicationAboutToSubmitHandler.handle(FinremCallbackRequest.builder()
-                .eventType(EventType.AMEND_APP_DETAILS)
-                .caseDetails(FinremCaseDetails.builder()
-                    .id(123L)
-                    .data(FinremCaseData.builder()
-                        .consentOrder(CaseDocument.builder()
-                            .documentUrl(DOC_URL)
-                            .documentFilename(FILE_NAME)
-                            .documentBinaryUrl(BINARY_URL)
-                            .build())
-                        .build())
+            .eventType(EventType.AMEND_APP_DETAILS)
+            .caseDetails(FinremCaseDetails.builder()
+                .id(123L)
+                .data(FinremCaseData.builder()
+                    .consentOrder(TestSetUpUtils.caseDocument())
                     .build())
-            .build(),"" );
-        Assert.assertEquals(DOC_URL, response.getData().getLatestConsentOrder().getDocumentUrl());
-        Assert.assertEquals(BINARY_URL, response.getData().getLatestConsentOrder().getDocumentBinaryUrl());
-        Assert.assertEquals(FILE_NAME, response.getData().getLatestConsentOrder().getDocumentFilename());
+                .build())
+            .build(), "");
+        Assert.assertNotNull(response.getData().getLatestConsentOrder().getDocumentUrl());
+
     }
 }
+
